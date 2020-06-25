e78155d7400383552275c171204b8ff62d60b7f4
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-25 15:51:04 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 def catch_phrase(phrase="It's-a me, Mario!\n")
+=======
+def 
+>>>>>>> bc9fb58ab7e0f7671f40fe29a684d5b86a4fb212
   puts phrase
 end
--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -1,6 +1,10 @@
 require "spec_helper"
 
+<<<<<<< HEAD
 describe (phrase="#catch_phrase") do 
+=======
+describe ("#catch_phrase") do 
+>>>>>>> bc9fb58ab7e0f7671f40fe29a684d5b86a4fb212
   it"puts out a catch phrase" do 
     phrase = "It's-a me, Mario!"
     expect{catch_phrase}.to output("It's-a me, Mario!\n").to_stdout
